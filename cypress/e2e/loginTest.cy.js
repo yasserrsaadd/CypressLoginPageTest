2103152e53b150bb3bee86a023f6f7bee58449f6
Yasser Saad <yassersaad795@gmail.com>
2025-08-27 22:27:48 +0300

    First-commiti

---
+++ cypress/e2e/loginTest.cy.js
@@ -0,0 +1,50 @@
+describe('Login page test', () => {
+  // testing login with valid credentials
+  beforeEach(() => {
+      cy.visit('https://practicetestautomation.com/practice-test-login/')  
+    })
+
+  it('should login successfully with valid credentials', () => {
+    cy.get('#username').type('student')
+    cy.get('#password').type('Password123')
+    cy.get('#submit').click()
+    cy.url().should('contain', 'https://practicetestautomation.com/logged-in-successfully/')
+    cy.contains('Logged In Successfully').should('be.visible')
+  })
+
+  //testing Logout functionality
+  it("should logout successfully", () => {
+  cy.get('#username').type('student')
+  cy.get('#password').type('Password123')
+  cy.get('#submit').click()
+
+  cy.get('.wp-block-button__link').click()
+  cy.url().should('contain', 'https://practicetestautomation.com/')
+})
+
+
+
+  // testing login with invalid Username
+  it('should display error message with invalid credentials', () => { 
+    cy.get('#username').type('incorrectUser')
+    cy.get('#password').type('Password123 ')
+    cy.get('#submit').click()
+    cy.get('#error').should('be.visible').and('contain', 'Your username is invalid!')
+  })
+
+  /// testing login with invalid Password
+  it('should display error message with invalid password', () => { 
+    cy.get('#username').type('student')
+    cy.get('#password').type('incorrectPassword')
+    cy.get('#submit').click()
+    cy.get('#error').should('be.visible').and('contain', 'Your password is invalid!')
+  })
+
+  // testing login with empty fields
+  it('should display error message when fields are empty', () => { 
+    cy.get('#submit').click()
+    cy.get('#error').should('be.visible').and('contain', 'Your username is invalid!')
+  })
+
+  
+})
